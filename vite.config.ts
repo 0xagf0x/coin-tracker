02097238c523ec5f8406905523ac57274390a4e9
charlie b <charles.buckley@mobelux.com>
2025-01-02 15:11:52 -0500

    updates vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/coin-tracker/',
-  plugins: [react()]
+  build: {
+    outDir: 'dist', // Default output directory
+  },
+  plugins: [react()],
 })
